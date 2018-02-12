cb8c4272ceb03c3f722ea5fc4fe1b1af2a2e20a6
Dan Erazo <dan.erazog@gmail.com>
2018-02-11 20:18:25 -0500

    Se modifica campos de clase Persona

--- Persona.java
+++ Persona.java
@@ -1,24 +1,24 @@
 public class Persona {
-    protected long codigo;
-	protected String nombre;
+    protected long codigoPersona;
+	protected String nombrePersona;
 	
 	public Persona(){
 		
 	}
 	
 	public void setCodigo(long codigo){
-		this.codigo = codigo;
+		this.codigoPersona = codigo;
 	}
 	
 	public void setNombre(String nombre){
-		this.nombre = nombre;
+		this.nombrePersona = nombre;
 	}
 	
 	public long getCodigo(){
-		return this.codigo;
+		return this.codigoPersona;
 	}
 	
 	public String getNombre(){
-		return this.nombre;
+		return this.nombrePersona;
 	}
 }
